b3aed413fef18d9108eed8e35fa83acd098e0536
Dongbo Wang <dongbow@microsoft.com>
2022-09-07 13:53:16 -0700

    Use `AssemblyLoadContextProxy` instead of `Assembly.LoadFile` (#11)

--- Targeting-NetStandard2.0/scenario-2/resolver/Class1.cs
+++ Targeting-NetStandard2.0/scenario-2/resolver/Class1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Management.Automation;
 using System.Reflection;
@@ -7,9 +8,21 @@ namespace assembly.resolver
 {
     public class Init : IModuleAssemblyInitializer, IModuleAssemblyCleanup
     {
-        private static string dependencyFolder = Path.Combine(
-            Path.GetDirectoryName(typeof(Init).Assembly.Location),
-            "Dependencies");
+        private static readonly string s_dependencyFolder;
+        private static readonly HashSet<string> s_dependencies;
+        private static readonly AssemblyLoadContextProxy s_proxy;
+
+        static Init()
+        {
+            s_dependencyFolder = Path.Combine(Path.GetDirectoryName(typeof(Init).Assembly.Location), "Dependencies");
+            s_dependencies = new(StringComparer.Ordinal);
+            s_proxy = AssemblyLoadContextProxy.CreateLoadContext("platyPS-load-context");
+
+            foreach (string filePath in Directory.EnumerateFiles(s_dependencyFolder, "*.dll"))
+            {
+                s_dependencies.Add(AssemblyName.GetAssemblyName(filePath).FullName);
+            }
+        }
 
         public void OnImport()
         {
@@ -21,24 +34,70 @@ namespace assembly.resolver
             AppDomain.CurrentDomain.AssemblyResolve -= ResolvingHandler;
         }
 
+        private static bool IsAssemblyMatching(AssemblyName assemblyName, Assembly requestingAssembly)
+        {
+            // The requesting assembly is always available in .NET, but could be null in .NET Framework.
+            // - When the requesting assembly is available, we check whether the loading request came from this
+            //   module (the 'conflict' assembly in this case), so as to make sure we only act on the request
+            //   from this module.
+            // - When the requesting assembly is not available, we just have to depend on the assembly name only.
+            return requestingAssembly is not null
+                ? requestingAssembly.FullName.StartsWith("conflict,") && s_dependencies.Contains(assemblyName.FullName)
+                : s_dependencies.Contains(assemblyName.FullName);
+        }
+
         internal static Assembly ResolvingHandler(object sender, ResolveEventArgs args)
         {
-            string name = args.Name;
-            if (name.Equals("Newtonsoft.Json, Version=13.0.0.0, Culture=neutral, PublicKeyToken=30ad4fe6b2a6aeed", StringComparison.OrdinalIgnoreCase))
+            var assemblyName = new AssemblyName(args.Name);
+            if (IsAssemblyMatching(assemblyName, args.RequestingAssembly))
             {
-                Console.WriteLine($"<*** Fall in 'ResolvingHandler': Newtonsoft.Json, Version=13.0.0.0  -- Loaded! ***>");
+                string fileName = assemblyName.Name + ".dll";
+                string filePath = Path.Combine(s_dependencyFolder, fileName);
 
-                string path = Path.Combine(dependencyFolder, name.Split(',')[0]) + ".dll";
-                if (File.Exists(path))
+                if (File.Exists(filePath))
                 {
-                    // When running in .NET, the 'LoadFile' API uses an anonymous AssemblyLoadContext to load the assembly file.
-                    // But it maintains a cache to guarantee that the same assembly instance is returned for the same assembly file path.
-                    // For details, see the .NET code at https://source.dot.net/#System.Private.CoreLib/Assembly.cs,239
-                    return Assembly.LoadFile(path);
+                    Console.WriteLine($"<*** Fall in 'ResolvingHandler': Newtonsoft.Json, Version=13.0.0.0  -- Loaded! ***>");
+                    // - In .NET, load the assembly into the custom assembly load context.
+                    // - In .NET Framework, assembly conflict is not a problem, so we load the assembly
+                    //   by 'Assembly.LoadFrom', the same as what powershell.exe would do.
+                    return s_proxy is not null
+                        ? s_proxy.LoadFromAssemblyPath(filePath)
+                        : Assembly.LoadFrom(filePath);
                 }
             }
 
             return null;
+        }
+    }
+
+    internal class AssemblyLoadContextProxy
+    {
+        private readonly object _customContext;
+        private readonly MethodInfo _loadFromAssemblyPath;
+
+        private AssemblyLoadContextProxy(Type alc, string loadContextName)
+        {
+            var ctor = alc.GetConstructor(new[] { typeof(string), typeof(bool) });
+            _loadFromAssemblyPath = alc.GetMethod("LoadFromAssemblyPath", new[] { typeof(string) });
+            _customContext = ctor.Invoke(new object[] { loadContextName, false });
+        }
+
+        internal Assembly LoadFromAssemblyPath(string assemblyPath)
+        {
+            return (Assembly) _loadFromAssemblyPath.Invoke(_customContext, new[] { assemblyPath });
+        }
+
+        internal static AssemblyLoadContextProxy CreateLoadContext(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+
+            var alc = typeof(object).Assembly.GetType("System.Runtime.Loader.AssemblyLoadContext");
+            return alc is not null
+                ? new AssemblyLoadContextProxy(alc, name)
+                : null;
         }
     }
 }
